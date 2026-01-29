48b364bea3309e8458685e4b1e3cc1ad19c827a0
vibhutegayatri <vibhutegayatri2004@gmail.com>
2026-01-30 02:07:30 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,27 @@
 
 
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import orderRoutes from "./routes/order.js";
+
+// dotenv.config();
+// const app = express();
+// connectDB();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -18,8 +40,11 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("✅ Server is running!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-
-
 
